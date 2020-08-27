bc58987d6bf7a635f07c02a52c6f979f7a8ede4f
Jonathan Daniel <jonathans.daniel@yahoo.com>
2020-08-27 16:00:42 -0500

    Initial Commit

---
+++ Car Payment Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Car Payment Calculator
+//
+//  Created by Jonathan Daniel on 8/27/20.
+//  Copyright © 2020 Jonathan Daniel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
